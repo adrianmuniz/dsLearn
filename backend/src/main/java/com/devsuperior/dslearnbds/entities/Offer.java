3cb77ea1e3e520e466ea5fcd945edc472ea35c6f
Cainã Adrian Muniz <adrianmuniz.ceo@outlook.com>
2022-11-28 20:42:46 -0300

    ManyToOne e OneToMany

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -4,14 +4,28 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_offer")
 public class Offer implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String edition;
 	private Instant startMoment;
 	private Instant endMoment;
 	
+	@ManyToOne
+	@JoinColumn(name = "course_id")
 	private Course course;
 	
 	public Offer() {
